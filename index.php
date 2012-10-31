526429b0c140c173eb8aaf559225865a7432897f
alexwilson2012 <alexwilson2012@u.northwestern.edu>
2012-10-31 02:29:53 -0500

    Added Events At NU and Happiness Club projects, fixed css

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <html>
 <head>
-    <title>Alex Wilson</title>
+	<title>Alex Wilson</title>
+
+<!-- CSS FILE -->
+<link type="text/css" rel="stylesheet" href="main.css">
+<!-- JAVASCRIPT FILE -->
+<script type="text/javascript" src="main.js"></script>
+
 </head>
 <body>
-<h1>Hi, I'm Alex!</h1>
-<h2>Welcome to my new website!</h2>
-<img src='images/alex2.jpg'/>
+<div id='main'>
+	<div id='header'>Hi, I'm Alex!</div>
+	<!-- <div style="background: black; height: 1px;"></div> -->
+	<div>Welcome to my new website!</div><br>
+	<img width='40%' style="border-radius: 20px; box-shadow: 5px 5px 15px black;" src='images/alex.jpg'><br><br><br><br>
+
+	<span>Some things that I am proud of...</span><br>
+	<a href='http://eventsatnu.com' target='_blank'><img width='20%' src='images/eventsatnu_logo.png'></a>
+	<a href='http://happinessclub.org' target='_blank'><img width='20%' src='images/happinessclub.png'></a>
+</div>
+
 </body>
 </html>
