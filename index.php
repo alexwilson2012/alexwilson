fda3aae0374e416ad38870878136336e2bb7cf44
Alex Wilson <AlexWilson2012@u.northwestern.edu>
2013-01-29 23:34:07 +0000

    fix link to inTouch

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 	<span id='apps'>
 		<a href='http://eventsatnu.com' target='_blank'><img width='20%' src='images/eventsatnu_logo.png'></a>
 		<a href='http://happinessclub.org' target='_blank'><img width='20%' src='images/happinessclub.png'></a>
-		<a href='http://ec2-54-242-115-65.compute-1.amazonaws.com' target='_blank'><img style='width: 20%' src='images/intouch2.png'></a>
+		<a href='http://ec2-54-242-115-65.compute-1.amazonaws.com/In-Touch-Native/www/login.html' target='_blank'><img style='width: 20%' src='images/intouch2.png'></a>
 		<a href='http://alexdeanwilson.org/grocerypad' target='_blank'><img style='width: 20%' src='images/grocerypad2.png'></a>
 		<!-- ; border-radius: 30px; box-shadow: 5px 5px 15px black -->
 	</span>
